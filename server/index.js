3ab1ee8debc7008514ebcb1deb0878cfe6a9c6a0
Craig Nockels <cnockels@gmail.com>
2020-04-23 17:03:32 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const PORT = 4000;
+
+express()
+  .use(function(req, res, next) {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  })
+  // present by default
+  .use(morgan('tiny'))
+  .use(express.static('./server/assets'))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .use('/', express.static(__dirname + '/'))
+
+  // endpoints
+  
+
+  .listen(PORT, () => console.info(`Listening on port ${PORT}`));

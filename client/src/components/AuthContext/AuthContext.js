56965aaf714a3fcf7b5feb992b247ec2e46b1532
Craig Nockels <cnockels@gmail.com>
2020-05-12 16:54:05 -0400

    bugfix

--- client/src/components/AuthContext/AuthContext.js
+++ client/src/components/AuthContext/AuthContext.js
@@ -21,7 +21,7 @@ export const AuthContext = createContext(null);
     projectId: process.env.REACT_APP_FB_PROJECTID,
     storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP_FB_MESSAGINSENDERID,
-    appId: process.env.REACT_APP_FB_APPID
+    appId: process.env.REACT_APP_FB_APPID,
   };
 
 //initializes firebase app
